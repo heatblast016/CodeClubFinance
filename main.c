c76607734b0c06424efea926d12b9b9294aeea17
heatblast016 <adiddee@live.com>
2016-11-07 14:23:07 -0800

    woot

--- main.c
+++ main.c
@@ -6,5 +6,11 @@ int main(void)
 {
     //open file
     FILE* inputfile = fopen("atmlog.atml", "w+");
+    char name[20];
+    for(int i = 65; i <= 85; i++)
+    {
+        name[i-65] = i;
+    }
+    create(name, inputfile);
     fclose(inputfile);
 }
