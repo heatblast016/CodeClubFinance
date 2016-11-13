4ecd4012731d16f3834b90a6c30b5407465040dc
heatblast016 <adiddee@live.com>
2016-11-13 14:18:35 -0800

    Depositing is DONE

--- funcs.c
+++ funcs.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include "funcs.h"
 void create(char name[20], FILE* ptr)
 {
@@ -13,18 +14,35 @@ void create(char name[20], FILE* ptr)
     fseek(ptr, 0, SEEK_END);
     fwrite(&tmp, sizeof(account), 1, ptr);
 }
-/*
+
 void deposit(char name[20], double amount, FILE* ptr)
 {
+    int trigger = 0;
     account tmp;
     fseek(ptr, 0, SEEK_SET);
-    while(fread(&ptr, sizeof(account), 1, ptr)
+    while(!feof(ptr))
     {
-        fread(&ptr, sizeof(account), 1, ptr);
-        
+         fread(&tmp, sizeof(account), 1, ptr);
+         if(strcmp(tmp.name, name))
+         {
+             trigger == 1;
+             break;
+         }
+    }
+    if(trigger == 0)
+    {
+        printf("ERROR: ACCOUNT NOT FOUND");
+    }
+    else
+    {
+        int x = sizeof(account);
+        x *= -1;
+        fseek(ptr, x, SEEK_CUR);
+        tmp.balance += amount;
+        fwrite(&tmp, sizeof(account), 1, ptr);
     }
 }
-*/
+
 void withdraw(char name[20], double amount, FILE* ptr)
 {
 
